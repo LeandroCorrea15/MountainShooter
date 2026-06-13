d2aae41e8e8f7c58d07889ea47605a495a3b37a5
Leandro <leandro_le@outlook.com>
2026-06-13 15:10:06 -0300

    SQLite3 implementation for Score using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,7 @@
 # C
 import pygame
 from pygame.constants import K_DOWN
+from pygame.examples.grid import WINDOW_WIDTH
 
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
@@ -132,8 +133,25 @@ SPAWN_TIME = 4000
 
 # T
 TIMEOUT_STEP = 100 #100ms
-TIMEOUT_LEVEL = 10000 #10s
+TIMEOUT_LEVEL = 20000 #10s
 
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
